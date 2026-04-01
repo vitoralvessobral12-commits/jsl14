49d60b72d9dc31da51e07263ef030f1bc50b9621
vitoralvessobral12-commits <vitor.alvessobral.12@gmail.com>
2026-04-01 19:16:21 -0300

    adiçao do salario e notas

---
+++ digital-college/JS-aula-2/NotANumber.js
@@ -0,0 +1,15 @@
+/*
+
+let salario = Number(prompt('qual seu salario '))
+
+let acrescimo = salario * 0.2
+let novosalario = salario + acrescimo
+ Number(console.log(novosalario))*/
+
+ let x = parseFloat(prompt("digite sua primeira nota"))
+let y = parseFloat(prompt("digite sua segunda nota"))
+let z = parseFloat(prompt("digite sua terceira nota"))
+
+let resultado = (x+y+z) / 3
+
+prompt(resultado)
