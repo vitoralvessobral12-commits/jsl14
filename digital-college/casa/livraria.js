0adc205bf4c2a8f705f069aed951142b139eee46
vitoralvessobral12-commits <vitor.alvessobral.12@gmail.com>
2026-04-21 21:01:10 -0300

    resolução1

---
+++ digital-college/casa/livraria.js
@@ -0,0 +1,63 @@
+
+let Livraria = {
+    nome: "Livraria do Cleitinho",
+    endereco: "Rua do Cleitinho, 123",
+    livros: []
+}
+
+let modeloLivro = {
+    titulo: "titulo do livro",
+    autor: "autor do livro",
+    ano: 2020,
+    editora: "editora do livro",
+    preco:10, 
+    estoque: 10
+}
+
+let livro1 = {
+    ...modeloLivro,
+    titulo: "percy jackson",
+    autor: "vitor alves",
+     editora: "jorge maluquices"
+}
+let livro2 = {
+    ...modeloLivro,
+    titulo: "joão e o pé de feijão",
+    autor: "vitor alves",
+     editora: "cleitinho lindex"
+}
+let livro3 = {
+    ...modeloLivro,
+    titulo: "a morte da bezerra",
+    autor: "cleitinho de assis",
+     editora: "sintonia arrochada"
+}
+console.log(Livraria)
+Livraria.livros.push(livro1,livro2,livro3)
+
+console.log(Livraria)
+//Atividade 1
+// Com base no objeto modeloLivro, crie 3 novos objetos representando livros diferentes e adicione-os ao array de livros da livraria. Use a desestruturação para criar os novos objetos a partir do modeloLivro.
+//para adicionar os livros na propriedade livros da livraria, poderá usar os métodos de arrays. 
+
+
+//Atividade 2
+//Use o laço for of e o for in para exibir as informações de cada livro da livraria. O for of deve ser usado para iterar sobre o array de livros, enquanto o for in deve ser usado para iterar sobre as propriedades de cada livro. Exiba o título, autor e preço de cada livro em uma string formatada. Ex: "O livro X do autor Y custa Z reais".
+
+
+//Atividade3 
+// Converta o objeto da livraria para JSON usando o método JSON.stringify() e exiba o resultado no console.
+
+
+//Atividade 4
+//Você recebeu o JSON de livro de uma livraria concorrente. O Cleitinho quer adicionar em sua livraria com um preço e um estoque melhor. Reduza o preço em 30% e tenha do dobro do estoque. Converta o JSON para objeto, faça as alterações necessárias e adicione o livro ao array de livros da livraria. Por fim, exiba a livraria atualizada no console. 
+
+const livroConcorrente = `{
+  "titulo": "JavaScript: O Guia Definitivo",
+  "autor": "David Flanagan",
+  "ano": 2020,
+  "editora": "Novatec",
+  "preco": 129.9,
+  "estoque": 25
+}`
+
